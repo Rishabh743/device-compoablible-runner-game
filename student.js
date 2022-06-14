4ce18874bcb718676fb105fe65477e9761c02bfb
Rishabh743 <102633583+Rishabh743@users.noreply.github.com>
2022-06-14 17:20:36 -0400

    Add files via upload

---
+++ student.js
@@ -0,0 +1,17 @@
+
+class Student
+{
+   constructor(name,age,grade){
+       this.name=name
+        this.age=age
+        this.grade=grade
+   }
+   show() {
+       console.log("student name",this.name)
+       console.log("student age",this.age)
+       console.log("student grade",this.grade)
+   }
+}
+
+
+
